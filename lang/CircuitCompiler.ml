945564158ee741a5a21d50ae69a1b6a6847088dc
sachour <sachour@mit.edu>
2015-06-24 10:21:03 -0400

    Added

--- lang/CircuitCompiler.ml
+++ lang/CircuitCompiler.ml
@@ -53,34 +53,66 @@ struct
       (System.to_string t.s)
 end
 
+
 module CircuitCompiler : MetaLanguageVisitor with type s = tbl  = 
 struct
    type s=tbl
-   let visit_rule (st:s) (r:rule) (rel:id) (inps:(string*id) list) (out:(string*id)) : s = 
+   let mangle_action_name (a:action) : string =
+         match (a.t, a.name,a.inputs, a.output) with
+         | (Action(tn,tins,rel,rules,tout),actName,actIns,actOuts) ->
+            let action_name = sanitize actName and action_kind = sanitize tn in
+            "act_"^action_kind^"_"^action_name
+         | _ -> raise (CircuitCompilationException ("failed to mangle action"))
+
+   let mangle_param_name (p:parameter) : string = 
+      match (p.name, p.value) with
+      | (name,value) -> let san_name = sanitize name in
+         "param_"^san_name
+      | _ -> raise (CircuitCompilationException ("failed to mangle action"))
+
+   let mangle_state_name (s:state) : string = 
+      match (s.t, s.name) with
+      | (State(kind), name) -> 
+         let san_kind = sanitize kind and san_name = sanitize name in
+         "st_"^san_kind^"_"^san_name
+      | (Signal(kind), name) ->
+         let san_kind = sanitize kind and san_name = sanitize name in
+         "sig_"^san_kind^"_"^san_name
+      | _ -> raise (CircuitCompilationException ("failed to mangle action"))
+
+
+   let visit_rule (st:s) (rname:string) (r:rule) (rel:id) (inps:(string*id) list) (out:(string*id)) : s = 
       st
 
-   let rec visit_rules (st:s) (rlst:rule list) (rel:id) (inps:(string*id) list) (out:(string*id)) : s =
+   let rec visit_rules (st:s) (rlst:(string*rule) list) (rel:id) (inps:(string*id) list) (out:(string*id)) : s =
       match rlst with
-      | h::t -> let e = visit_rule st h rel inps out in visit_rules e t rel inps out 
+      | (name,rul)::t -> let e = visit_rule st name rul rel inps out in visit_rules e t rel inps out 
       | [] -> st
+
+   let rec visit_relation (st:s) (i:id) (rel:relation) (inps:(string*id) list) (out:(string*id)) : s =
+      let (oname,oid) = out in
+      let elem = SMTHole(i,[],Integer(0),[],None) in
+      match System.get_circuit st.s oid with
+      | Some(circ) ->
+         let circ = Circuit.add_block circ elem in
+         CircuitTable.update_circuit st circ
+      | None -> raise (CircuitCompilationException ("expected to find circuit with name "^oname))
 
    let visit_action (st:s)  (act:action) : s  = 
       match (act.t, act.name,act.inputs, act.output) with
       | (Action(tn,tins,rel,rules,tout),actName,actIns,actOuts) ->
-         let an = sanitize actName in
-         let kind = sanitize tn in
-         let smtid = (CircuitTable.fId, Some(kind^"_"^an)) in
-
-         st
+         let smtid = (CircuitTable.fId st, Some (mangle_action_name act)) in
+         let st = visit_relation st smtid rel [] ("",smtid) in
+         visit_rules st rules smtid [] ("",smtid) 
       | _ -> raise (CircuitCompilationException "unexpected parameter")
 
-   let visit_state (st: s) (state:state) : s  = 
+   let visit_state (st: s) (state:state) : s  =
+      let name = mangle_state_name state in 
       match state with
          | {name=name; t=Signal(v)} -> 
-            let n = sanitize name in
             let 
-               join_ins_id = (CircuitTable.fId st, Some ("join_ins_"^n))
-               and join_outs_id = (CircuitTable.fId st, Some ("join_outs_"^n))
+               join_ins_id = (CircuitTable.fId st, Some (name^"_JOUT"))
+               and join_outs_id = (CircuitTable.fId st, Some (name^"_JIN"))
             in
             let
                w_ins_outs = Wire(CircuitTable.make st,join_ins_id,join_outs_id)
@@ -89,7 +121,7 @@ struct
                j_ins = Joint(join_ins_id, [w_ins_outs])
                and j_outs = Joint(join_outs_id, [w_ins_outs])
             in
-            let circ = Circuit.create (CircuitTable.fId st, Some n) in
+            let circ = Circuit.create (CircuitTable.fId st, Some name) in
             let circ = Circuit.add_blocks circ [j_ins;j_outs] in
             let circ = Circuit.add_wires circ [w_ins_outs] in
             let circ = Circuit.add_inputs circ [("in",join_ins_id)] in
@@ -97,13 +129,12 @@ struct
                CircuitTable.add_circuit st circ
 
          | {name=name; t=State(v)} -> 
-            let n = sanitize name in
             let 
-               capac_id = (CircuitTable.fId st, Some ("cap_"^n))
-               and join_hub_id = (CircuitTable.fId st, Some ("join_hub_"^n))
-               and join_use_id = (CircuitTable.fId st, Some ("join_use_"^n))
-               and join_add_id = (CircuitTable.fId st, Some ("join_add_"^n))
-               and gnd_id = (CircuitTable.fId st, Some ("gnd_"^n)) in
+               capac_id = (CircuitTable.fId st, Some (name^"_CAP"))
+               and join_hub_id = (CircuitTable.fId st, Some (name^"_JHUB"))
+               and join_use_id = (CircuitTable.fId st, Some (name^"_JUSE"))
+               and join_add_id = (CircuitTable.fId st, Some (name^"_JADD"))
+               and gnd_id = (CircuitTable.fId st, Some (name^"_GND")) in
             let 
                gnd = Ground(gnd_id);
                and capac = Capacitor(capac_id,1.)
@@ -117,7 +148,7 @@ struct
                and j_add = Joint(join_add_id, [w_hub_add])
                and j_use = Joint(join_use_id, [w_hub_use])
             in
-            let circ = Circuit.create (CircuitTable.fId st, Some n) in
+            let circ = Circuit.create (CircuitTable.fId st, Some name) in
             let circ = Circuit.add_blocks circ [j_hub;j_use;j_add;capac;gnd] in
             let circ = Circuit.add_wires circ [w_capac_gnd;w_capac_hub;w_hub_use;w_hub_add] in
             let circ = Circuit.add_inputs circ [("use",join_use_id); ("gen",join_add_id)] in
@@ -128,7 +159,7 @@ struct
    let visit_parameter (st: s) (p:parameter) : s  =
       match p with
          | {name=n; value=v; t=ty} ->
-            CircuitTable.add_parameter st n v 
+            CircuitTable.add_parameter st (mangle_param_name p) v 
          | _ -> raise (CircuitCompilationException "unexpected parameter")
       
       
