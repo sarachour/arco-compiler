cb40ffaeb6db10942ad672c25f3ca2b3d3f72bda
sachour <sachour@mit.edu>
2015-06-25 12:53:12 -0400

    Added solver

--- solver/equiv.py
+++ solver/equiv.py
@@ -2,10 +2,12 @@ from z3 import *;
 
 x = Int('x')
 y = Int('y')
+k = Int('k')
+
+
 s = Solver()
-s.add(x > 0)
-s.add(x < 2)
-s.add(y == x + 1)
+s.add(x / (x + y) == x*k/(x*k + k))
+s.add(k >= 0)
 
 res = s.check()
 m = s.model()
