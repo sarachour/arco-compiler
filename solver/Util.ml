5e8e2ca3792f83c7c41b67d30f778ad15e627a55
sachour.desktop <sachour@mit.edu>
2015-06-28 13:33:40 -0400

    Added overlay package

---
+++ solver/Util.ml
@@ -0,0 +1,23 @@
+open ExtString
+
+type 'a maybe = 
+   Some of 'a 
+   | None
+;;
+
+module type GENERIC_TYPE = sig type t end
+
+exception TypeException of string;;
+exception GraphException of string;;
+exception PrintException of string;;
+
+let sanitize s : string = 
+  let rec _sanitize (lst: char list) (cap:bool): char list = 
+    match (lst,cap) with
+      | ('_'::t,_) -> _sanitize t true
+      | ('-'::t,_) -> _sanitize t true
+      | (x::t,true) -> '_'::(x)::(_sanitize t false)
+      | (x::t,false) -> (x)::(_sanitize t false)
+      | ([],_) -> []
+  in
+    ExtString.String.implode (_sanitize (ExtString.String.explode s) false)
