11a70de00b36a213c8bea7bd31feee28a8cf0c97
sachour <sachour@mit.edu>
2015-06-30 11:02:09 -0400

    Added gitignore

---
+++ solver/GenToHW.ml
@@ -0,0 +1,19 @@
+open HWLib
+open HWData
+open Generic
+
+type cvanalogy = Current | Voltage
+
+
+module HWConv : 
+sig
+   val convert : hwarch -> grel -> cvanalogy -> hwconfig
+end =
+struct
+   let convert c r algy= 
+      let c = HWArch.create_config() in
+      c
+
+
+
+end
