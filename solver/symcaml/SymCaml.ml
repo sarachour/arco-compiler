46d8b64d1979ee895f4958c03afcc710d38730fd
sachour <sachour@mit.edu>
2015-07-03 13:54:03 -0400

    Some basic simplification

--- solver/symcaml/SymCaml.ml
+++ solver/symcaml/SymCaml.ml
@@ -20,12 +20,16 @@ sig
    val init : unit -> symcaml
    val print_info : unit -> unit
    val define_symbol : symcaml ->  string -> spy_expr
-   val define_expr : symcaml -> string -> spy_expr -> unit
-   val expand : symcaml -> spy_expr_or_var -> spy_expr
+   val define_expr : symcaml -> string -> spy_expr -> spy_expr
    val print_var : symcaml ->  string -> unit
    val get_var : symcaml ->  string -> spy_expr
    val clear : symcaml -> unit
    val expr2py : spy_expr -> string
+
+
+   val expand : symcaml -> spy_expr_or_var -> spy_expr
+   val simpl : symcaml -> spy_expr_or_var -> spy_expr
+   val eval : symcaml -> spy_expr_or_var -> spy_expr
 end = 
 struct 
    type symcaml = {
@@ -45,10 +49,10 @@ struct
    let _env (n:string) : string = "env[\""^n^"\"]"
 
    let _toexpr (s:symcaml) (n:string) : spy_expr = 
-      pyrun_simplestring("__token__=srepr("^(_env n)^")");
+      let _ = pyrun_simplestring("__token__=srepr("^(_env n)^")") in
       let canonical = pydict_getitemstring(s.env, "__token__") in
       let cstr = pystring_asstring (pyobject_repr canonical) in 
-      Printf.printf "[%s]\n" cstr; 
+      (*Printf.printf "[%s]\n" cstr; *)
       try
          let lexbuf = Lexing.from_string cstr in
          let result = SymCamlParser.main SymCamlLexer.main lexbuf in
@@ -57,18 +61,19 @@ struct
          |SymCamlData.SymCamlParserError(msg) -> raise (SymCamlException ("parse error:"^msg))
          |_ -> raise (SymCamlException "failed.")
 
+   
    let init () =
       let modulename = "sympy" in 
       py_setprogramname("sympy.interp");
       py_initialize();
-      pyrun_simplestring("from sympy import *"); 
-      pyrun_simplestring("env = {}"); 
+      let _ = pyrun_simplestring("from sympy import *") in
+      let _ = pyrun_simplestring("env = {}") in 
       let mdl = pyimport_addmodule("__main__") in 
       let env = pymodule_getdict(mdl) in
       {mdl=mdl; env=env} 
 
    let clear s = 
-       pyrun_simplestring("env = {}"); ()
+       let _ = pyrun_simplestring("env = {}") in ()
 
    
    let rec expr2py (e:spy_expr) : string= 
@@ -92,31 +97,48 @@ struct
       | Decimal(x) -> string_of_float x
       | Integer(x) -> string_of_int x
 
+   let _rslv_var_expr f e : string = match e with
+         | Var(v) -> f (_env v)
+         | Expr(e) -> f (expr2py (Paren e))
+
    let define_symbol (s:symcaml) (x:string) : spy_expr =
-       pyrun_simplestring((_env x)^"= Symbol(\""^x^"\");"); (Symbol x)
+      let _ = pyrun_simplestring((_env x)^"= Symbol(\""^x^"\");"); in 
+      (Symbol x)
 
 
    let define_expr (s:symcaml) (x:string) (e:spy_expr) = 
       let expr  =(expr2py e) in 
-      pyrun_simplestring((_env x)^"="^expr); ()
-   
-   let expand (s:symcaml) (e:spy_expr_or_var) = 
-      let tmp = _env "__tmp__" in
-      let _ =
-         match e with
-         | Var(v) -> pyrun_simplestring(tmp^" ="^(_env v)^".expand()")
-         | Expr(e) -> pyrun_simplestring(tmp^" ="^(expr2py (Paren e))^".expand()")
-      in
-      _toexpr s "__tmp__"
+      let _ = pyrun_simplestring((_env x)^"="^expr) in 
+      Symbol(x)
 
    let get_var (s:symcaml) (x:string) : spy_expr = 
      _toexpr s x
 
    let print_var (s:symcaml) (x:string) = 
-      pyrun_simplestring("print srepr("^(_env x)^")"); ()
+      let _ = pyrun_simplestring("print srepr("^(_env x)^")") in 
+      ()
 
 
+   let expand (s:symcaml) (e:spy_expr_or_var) =
+      let ntmp = "__tmp__" in
+      let tmp = _env ntmp in
+      let cmd = _rslv_var_expr (fun x -> tmp^"="^x^".expand()") e in
+      pyrun_simplestring(cmd);
+      _toexpr s ntmp
+
+   let eval (s:symcaml) (e:spy_expr_or_var) = 
+      let ntmp = "__tmp__" in
+      let tmp = _env ntmp in
+      let cmd = _rslv_var_expr (fun x -> tmp^"="^x^".doit()") e in
+      pyrun_simplestring(cmd);
+      _toexpr s ntmp
    
+   let simpl (s:symcaml) (e:spy_expr_or_var) = 
+      let ntmp = "__tmp__" in
+      let tmp = _env ntmp in
+      let cmd = _rslv_var_expr (fun x -> tmp^"=simplify("^x^")") e in
+      pyrun_simplestring(cmd);
+      _toexpr s ntmp
 end
 
 let main () = 
@@ -124,11 +146,13 @@ let main () =
    SymCaml.define_symbol s "a";
    SymCaml.define_symbol s "b";
    SymCaml.define_symbol s "c";
-   SymCaml.define_expr s "e" (Exp(Add([Symbol("a");Symbol("b");Symbol("c")]), Integer(3)));
+   SymCaml.define_expr s "e" (Exp(Add([Symbol("a");Symbol("a");Symbol("c")]), Integer(3)));
    Printf.printf "-------\n";
    let res = SymCaml.get_var s "e" in
    Printf.printf "%s\n" (SymCaml.expr2py res);
    let res = SymCaml.expand s (Var "e") in
+   Printf.printf "%s\n" (SymCaml.expr2py res);
+   let res = SymCaml.simpl s (Var "e") in
    Printf.printf "%s\n" (SymCaml.expr2py res)
 ;;
 
