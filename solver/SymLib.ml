ba5600b82db5e6e3659c196594c7c87e0ea9dd13
sachour <sachour@mit.edu>
2015-07-07 13:06:34 -0400

    Added instantiations

--- solver/SymLib.ml
+++ solver/SymLib.ml
@@ -29,17 +29,22 @@ struct
       ns: string
    }
 
+   let mangle ns (l:hwliteral) = 
+      match l with 
+         | Current(x) -> ns^"_"^x^"."^"I"
+         | Voltage(x) -> ns^"_"^x^"."^"V"
+         | Parameter(x) -> ns^"_"^x^"."^"P"
+
    let load_env (w:SymCaml.symcaml maybe) (s:symenv) : SymCaml.symcaml =
       let env = match w with
          | None -> 
             let e = SymCaml.init() in
             let _ = SymCaml.clear e in
-            let _ = SymCaml.define_function e "V" in
-            let _ = SymCaml.define_function e "I" in
             e
          | Some(x) -> x
       in
-      let _ = SymCaml.define_function env s.ns in
+      List.iter (fun x -> SymCaml.define_wildcard env x; ()) s.wildcards;
+      List.iter (fun x -> SymCaml.define_symbol env x; ()) s.vars;
       env
 
 
@@ -56,14 +61,16 @@ struct
          | Exp(b,e) -> Exp(hwexpr2symexpr b, hwexpr2symexpr e)
          | NatExp(e) -> NatExp(hwexpr2symexpr e) 
          | Deriv(e) -> Deriv(hwexpr2symexpr e, [("t",1)])
-         | Literal(Current(x)) -> let nsx = Function(ns,[Symbol(x)]) in Function("I",[nsx])
          | Literal(Voltage(x)) -> let nsx = Function(ns,[Symbol(x)]) in Function("V",[nsx])
          | Literal(Parameter(x)) -> 
+            begin
             match List.filter (fun (n,v) -> n = x) h.params with
             | [(n,Some(v))] -> Decimal(v)
-            | [(n,None)] -> Function(ns,[Symbol(n)])
+            | [(n,None)] -> Symbol(mangle ns (Parameter x))
             | [] -> raise (SymLibException ("no parameter with name "^x))
             | _ -> raise (SymLibException ("too many parameters with name "^x))
+            end
+         | Literal(x) -> (Symbol(mangle ns x))
          | _ -> raise (SymLibException "unhandled symlib expr")
       in 
       let hwrel2symrel (r:hwrel) : symrel = match r with 
@@ -72,11 +79,11 @@ struct
       in
       let rec hwparam2symlst (r:(string*hwdecimal maybe) list) : string list = match r with 
          |(n,Some(vl))::t -> (hwparam2symlst t)
-         |(n,None)::t -> n::(hwparam2symlst t)
+         |(n,None)::t -> (mangle ns (Parameter n))::(hwparam2symlst t)
          |[] -> []
       in
       let rec hwidstrlst2symlst (r:(string*hwid) list) : string list = match r with
-         |(n,id)::t -> n::(hwidstrlst2symlst t)
+         |(n,id)::t -> (mangle ns (Voltage n))::(mangle ns (Current n))::(hwidstrlst2symlst t)
          | [] -> []
       in
       let s : symenv = {vars=[];wildcards=[];rels=[];ns=ns} in
