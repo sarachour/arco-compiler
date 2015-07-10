0b1965dfb875ab6f8d145b74f3b3ea656fb42ada
sachour <sachour@mit.edu>
2015-07-10 14:17:57 -0400

    Sufficient to print

--- solver/ConfigSearchDeriver.ml
+++ solver/ConfigSearchDeriver.ml
@@ -1,5 +1,5 @@
 open HWData
-
+open HWLib
 
 (*
    Given a partial hardware configuration (things partly connected with generic connections)
@@ -66,15 +66,58 @@ struct
 
    type goaltree = goalnode
 
-   let delta2str (d:delta) : string = ""
-   let goal2str (d:goal) : string = ""
-   let goaltree2str (d:goaltree) : string = ""
+   let __spacing = "  "
+
+   let rec _delta2str (prefix:string) (d:delta) : string = match d with 
+      | DUseComponent(hwcomp) -> let (n,id) = hwcomp.id in
+         prefix^"use component "^n^"("^(HWUtil.hwid2str id)^")"
+      | DAddWire(expr,src,Some(sink)) ->
+         let sexpr = HWUtil.hwexpr2str expr in 
+         prefix^sexpr^" : "^src^" -> "^sink
+      | DAddWire(expr,src,None) ->
+         let sexpr = HWUtil.hwexpr2str expr in 
+         prefix^sexpr^" : "^src^" -> ?"
+      | DAggregate(lst) -> 
+         List.fold_right (fun x r -> r^(_delta2str prefix x)^"\n") lst ""
+      | DNone -> "none"
+
+   let _goal2str (prefix:string) (d:goal) : string = 
+      let expr = HWUtil.hwrel2str (List.nth (d.value.constraints) 0) in 
+      match d.name with 
+         | Some(name) -> prefix^"goal "^(HWUtil.hwlit2str name)^" := "^expr
+         | None ->  prefix^"goal _ := "^expr
+
+   let rec _goaltree2str (prefix:string) (d:goaltree) : string = 
+      let _goallist2str lst = 
+         List.fold_right (fun x r -> r^(_goaltree2str (prefix^__spacing) x)) lst ""
+      in 
+      match d with 
+      | GNoSolutionNode -> prefix^"no solution\n"
+      | GUnsolvedNode(g) -> prefix^"unsolved "^(_goal2str "" g)^"\n"
+      | GSolutionNode(g,d,subs) -> 
+         (_goal2str prefix g)^" // "^(_delta2str "" d)^"\n"^
+         (_goallist2str subs)
+      | GMultipleSolutionNode(lst) -> 
+         prefix^"multiple solutions:\n"^
+         (_goallist2str lst)
+      | _ -> prefix^"unhandled\n"
+
+   let delta2str (d:delta) : string = _delta2str "" d 
+   let goal2str (d:goal) : string = _goal2str "" d
+   let goaltree2str (d:goaltree) : string = _goaltree2str "" d
 
    let make_goal (hl:hwliteral option) (hc:hwcomp) : goal = 
       {name=hl; value=hc}
 
    let traverse (gt:goaltree) (fn:goal->goalnode) (interactive:bool) : goaltree = 
-      let rep pref g = if interactive then Printf.printf "%s: %s\n" pref (goaltree2str g) else ()
+      let rep pref g = 
+         if interactive then 
+            begin
+            Printf.printf "%s: %s\n" pref (goaltree2str g);
+            flush_all()
+            end
+         else 
+            ()
       in
       let wait () =  
             if interactive then  
@@ -97,7 +140,7 @@ struct
             let newnode = fn x in 
             rep "solved node" newnode;
             wait();
-            _traverse g
+            _traverse newnode
          | GSolutionNode(g,d,lst) ->
             let nlst = List.map (fun x -> _traverse x ) lst in
             GSolutionNode(g,d,nlst)
