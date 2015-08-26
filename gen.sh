d0b3111742a914a2e7629eda2097fd34e3979d49
Sara Achour <sachour@gmail.com>
2015-08-26 16:23:59 -0400

    fixed thing

--- gen.sh
+++ gen.sh
@@ -3,6 +3,6 @@
 BIOCAD_PATH=$PWD
 
 echo "eval $(opam config env)"
-echo "export BIOCAD_HOME=$BIOCAD_PATH"
-echo 'export PATH=$BIOCAD_HOME/dep/z3/build:$BIOCAD_HOME/dep/dReal/bin:$PATH'
-echo 'export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$BIOCAD_HOME/z3/build'
+echo "export ARCO_HOME=$BIOCAD_PATH"
+echo 'export PATH=$ARCO_HOME/dep/z3/build:$ARCO_HOME/dep/dReal/bin:$PATH'
+echo 'export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ARCO_HOME/z3/build'
