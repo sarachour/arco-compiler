99a68fa05fb0299b40e9791b94b7b99969c48efa
Sara Achour <sachour@mit.edu>
2015-08-26 18:11:34 -0400

    fixed

--- gen.sh
+++ gen.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-BIOCAD_PATH=$PWD
 
 echo "eval $(opam config env)"
-echo "export ARCO_HOME=$BIOCAD_PATH"
+echo "export ARCO_HOME=$PWD"
 echo 'export PATH=$ARCO_HOME/dep/z3/build:$ARCO_HOME/dep/dReal/bin:$PATH'
 echo 'export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$ARCO_HOME/z3/build'
