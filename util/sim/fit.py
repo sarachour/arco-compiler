f7fb8bf7b75422f4c26c105d60fa1a105c78e83a
Sara Achour <sachour@mit.edu>
2015-09-18 05:54:50 -0400

    t

--- util/sim/fit.py
+++ util/sim/fit.py
@@ -21,15 +21,18 @@ def run(libdir, infile, outfile):
 	pfile = infile.split(".")[0] + ".spice-params"
 	pars = ParamFile.load(libdir + pfile);
 	spice.set_comp_vars(pars);
+	# Add Sweep experiment across each input for min to max
+	
 	arco.set_param("a",0.5);
 	
-	spec = open(name+".spac", "w");
-	exp = open("experiment.sim", "w");
-	comp = open(name+".ckt", "w");
-	
 	print("== Generating Spec ==");
+	spec = open(name+".spac", "w");
 	arco.gen_spec(spec);
+	
 	print("== Generating Component ==");
+	comp = open(name+".ckt", "w");
 	spice.gen_comp(comp);
+	
 	print("== Generating Experiment ==");
+	exp = open("experiment.sim", "w");
 	spice.gen_exp(libdir, exp);
--- util/test_sim.sh
+++ util/test_sim.sh
@@ -1,3 +1,3 @@
-./sim.py --t fit -l ../sim/libs/basic/ -i vinv.comp -o log.cfg
+./sim.py --t fit -l ../sim/libs/basic/ -i vinv.comp -o vinv.model-params
 echo "Running Experiment"
 ngspice -b experiment.sim 
