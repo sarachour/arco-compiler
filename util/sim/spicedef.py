ce430d8041db8750d0dc6d0a124e8caa8811d155
Sara Achour <sachour@mit.edu>
2015-09-17 23:47:30 -0400

    added parsing function

--- util/sim/spicedef.py
+++ util/sim/spicedef.py
@@ -5,5 +5,34 @@ class SpiceDef:
 	
 	def load(self,url):
 		fn = open(url,"r");
+		cmd = None;
+		self.inputs = [];
+		self.outputs = [];
+		self.spice = {};
+		self.spice["def"] = [];
+		self.spice["use"] = "";
+		self.model = "";
+		
 		for line in fn:
-			print(line);
+			if line.startswith("@"):
+				cmd = line.split("\n")[0].split("@")[1];
+			else:
+				if cmd == "inputs":
+					inp = line.split("\n")[0];
+					self.inputs.append(inp);
+					
+				elif cmd == "outputs":
+					outp = line.split("\n")[0];
+					self.outputs.append(outp);
+					
+				elif cmd == "model":
+					model = line.split("\n")[0];
+					self.model = model;
+				
+				elif cmd == "spice-use":
+					use = line.split("\n")[0];
+					self.spice["use"] = use;
+				
+				elif cmd == "spice-def":
+					defn = line.split("\n")[0];
+					self.spice["def"].append(defn);
