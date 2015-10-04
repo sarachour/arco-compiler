aace2fadcec04ab8781e8a21a32c4931dcf14dab
Sara Achour <sachour@mit.edu>
2015-10-04 11:24:17 -0400

    Added solver

--- solver-new/AST.ml
+++ solver-new/AST.ml
@@ -30,12 +30,12 @@ let error n msg = raise (ASTException(n,msg))
 
 module AST : sig
     val to_string : ('a ast_node) -> ('a -> string) -> string
-    val conv : ('a ast_node) -> ('a -> 'b) -> ('b ast_node)
+    val map : ('a ast_node) -> ('a -> 'b) -> ('b ast_node)
     val to_symcaml : ('a ast_node) -> ('a -> symvar) -> (symexpr)
 end =
 struct
     let to_string ast fn : string = error "to_string" "unimplemented"
-    let conv n fn  = error "conv" "unimplemented"
+    let map n fn  = error "conv" "unimplemented"
     let to_symcaml ast fn : symexpr = error "to_symcaml" "unimplemented"
 
 end
--- solver-new/Logic.ml
+++ solver-new/Logic.ml
@@ -4,18 +4,23 @@ exception LogicException of string*string
 let error n msg = raise (LogicException(n,msg))
 
 type ptr = int
+type 'a logic_node =
+  | End
+  | Data of 'a
+  | Memory of ptr
+
 type 'a logic =
   | Any of (module Set.S with type elt=('a logic))
   | All of (module Set.S with type elt=('a logic))
-  | End
-  | Node of 'a*('a logic)
-  | Memory of ptr
+  | Node of 'a logic_node
 
 
 module Logic : sig
   val to_string : 'a logic -> ('a -> string) -> string
+  val map : 'a logic -> ('a logic_node -> 'b logic_node) -> 'b logic_node
 end =
 struct
   let to_string l fn = error "to_string" "unimplemented"
+  let map fn = error "map" "unimplemented"
 
 end
