2f4e1f62fbc59ddddb3154c32a81ff70a5405b03
Sara Achour <sachour@mit.edu>
2015-10-14 14:38:29 -0400

    correctly parsers

--- solver/ParseGenUtil.ml
+++ solver/ParseGenUtil.ml
@@ -5,29 +5,32 @@ open AST
 exception Lex_error of string
 exception Parse_error of string
 
+
 let parse_lexbuf prefix plroutine lexbuf  =
+    let throw_error ex lex =
+      let pos = lexbuf.Lexing.lex_curr_p in
+      let line = pos.Lexing.pos_lnum in
+      let column = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
+      let token = if lex = false then Lexing.lexeme lexbuf else "?" in
+      let exstr =  Printexc.to_string ex in
+      let locstr = Printf.sprintf "%s|%d:%d: '%s'" prefix line column token in
+      let _ = Printf.printf "=========\n%s\n\n%s\n============\n" locstr exstr in
+      flush_all();
+      if lex then
+        raise (Lex_error(locstr^exstr))
+      else
+        raise (Parse_error(locstr^exstr))
+    in
     try
       plroutine lexbuf
     with
     | Failure e ->
       if e = "lexing: empty token" then
-        let pos = lexbuf.Lexing.lex_curr_p in
-        let line = pos.Lexing.pos_lnum in
-        let column = pos.Lexing.pos_cnum - pos.Lexing.pos_bol + 1 in
-        raise (Lex_error (Printf.sprintf "%s|%d:%d" prefix line column))
+        throw_error (Failure e) true
       else failwith e
-    | Parsing.Parse_error ->
-      let pos = lexbuf.Lexing.lex_curr_p in
-      let line = pos.Lexing.pos_lnum in
-      let column = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
-      let token = Lexing.lexeme lexbuf in
-      raise (Parse_error (Printf.sprintf "%s|%d:%d: '%s'" prefix line column token))
-    | Error ->
-      let pos = lexbuf.Lexing.lex_curr_p in
-      let line = pos.Lexing.pos_lnum in
-      let column = pos.Lexing.pos_cnum - pos.Lexing.pos_bol in
-      let token = Lexing.lexeme lexbuf in
-      raise (Parse_error (Printf.sprintf "%s|  %d:%d: '%s'" prefix line column token))
+    | e ->
+      throw_error e false
+
 let string_to_ast fn =
   let lb = Lexing.from_string fn in
   parse_lexbuf "ast" (fun x -> ParseAST.expr LexAST.expr x) lb
