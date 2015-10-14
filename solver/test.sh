9b6fbb99106ff10b622f486305c19f454d2cae33
Sara Achour <sachour@mit.edu>
2015-10-14 14:44:56 -0400

    fi

--- solver/test.sh
+++ solver/test.sh
@@ -4,4 +4,16 @@
 NAME=$1
 ANALOGY=$2
 
+if [ "$NAME" = "test-math-parse" ]; then
+  NAME=$2
+  make && ./solver -hwspec test/math-parser/empty.spec -formula test/math-parser/$NAME.math -analogy voltage
+  exit 0
+fi
+
+if [ "$NAME" = "test-hw-parse" ]; then
+  NAME=$2
+  make && ./solver -hwspec test/hw-parser/$NAME.spec -formula test/hw-parser/empty.math -analogy voltage
+  exit 0
+fi
+
 make && ./solver -hwspec test/$NAME.spec -formula test/$NAME.math -analogy $ANALOGY
