49a66e4ff276886375879ffb25718dbe180f93ff
Sara Achour <sachour@mit.edu>
2015-11-06 15:17:05 -0500

    t

---
+++ solver/SolverSearch.ml
@@ -0,0 +1,154 @@
+open Util
+open SolverData
+open SolverUtil
+open SolverSln
+open SolverData
+
+
+module SearchLib =
+struct
+  let step2str n = match n with
+  | SAddGoal(v) -> "add "^(UnivLib.urel2str v)
+  | SRemoveGoal(v) -> "rm "^(UnivLib.urel2str v)
+  | SAddNode(id,rels) -> "add "^(UnivLib.unodeid2name id)
+  | SRemoveNode(id) -> "rm"^(UnivLib.unodeid2name id)
+  | SSolUseNode(id,i) -> "SLN use "^(UnivLib.unodeid2name id)^"."^(string_of_int i)
+  | SSolAddConn(src,snk) -> "SLN mkconn "^(SlnLib.wire2str src)^" <-> "^(SlnLib.wire2str snk)
+  | _ -> "?"
+
+  let visited b n =
+    SET.has b.visited n.id
+
+  let visit b n =
+    let _ = b.visited <- SET.add b.visited n.id in
+    ()
+
+  let steps2str (b:buffer) (n:steps) =
+    let id = (if visited b n then "." else "?")^(string_of_int n.id) in
+    let prefix = match b.curr with
+    | Some(c) -> if c = n then "["^id^"]" else id
+    | None -> id
+    in
+    prefix^": "^(List.fold_right (fun x r -> r^" "^(step2str x)^";") n.s "")
+
+  let score2str e = "score"
+
+  let mksteps b =
+    let _ = (b.cnt <- b.cnt + 1) in
+    Some {s=[]; id=b.cnt}
+
+  let mkscore () =
+    1.
+
+  let start b =
+    b.step_buf <- mksteps b
+
+  let id2node b x =
+    match TREE.filter_nodes b.paths (fun q -> q.id = x) with
+    | [x] -> x
+    | _ -> error "id2node" "no node with that id exists."
+
+  let add_step b s =
+    match b.step_buf with
+    | Some(b) -> b.s <- s::b.s
+    | None -> ()
+
+  let commit b =
+    match b.step_buf, b.curr with
+    | Some(q), Some(c) ->
+      let _ = TREE.mknode b.paths q in
+      let _ = TREE.mkedge b.paths c q (mkscore()) in
+      let _ = b.step_buf <- None in
+      q
+    | Some(q), None ->
+      error "commit" "cannot commit new step with no parent."
+    | _,_ -> error "commit" "cannot commit empty node."
+
+
+  let rm b n =
+    let _ = match b.curr with
+    | Some(v) -> if v = n then let _ = b.curr <- None in ()
+    | None -> ()
+    in
+    let _ = TREE.rmnode b.paths n in
+    b
+
+
+
+  let buf2str b =
+    TREE.tostr b.paths (fun x -> (steps2str b x))
+
+
+  let cursor b =
+    match b.curr with
+    | Some(c) -> c
+    | None -> error "cursor" "expected cursor."
+
+
+  let apply_step (slvenv:slvr) (tbl:gltbl) (s:step) =
+    (*let _ = Printf.printf "> do step %s\n" (step2str s) in*)
+    match s with
+    | SAddGoal(g) -> let _ = SET.add tbl.goals g in ()
+    | SRemoveGoal(g) -> let _ = SET.rm tbl.goals g in ()
+    | SAddNode(id,u) -> let _ = MAP.put tbl.nodes id {name=(UnivLib.unodeid2name id);rels=SET.to_set u (fun x y -> x = y)} in ()
+    | SSolUseNode(id,i) -> let _ = SlnLib.usecomp_mark tbl.sln id i in ()
+    | SSolAddConn(src,snk) -> let _ = SlnLib.mkconn tbl.sln src snk in ()
+    | SSolAddLabel(wid, prop, lbl) -> let _ = SlnLib.mklabel tbl.sln wid prop lbl in ()
+
+  let apply_steps (slvenv:slvr) (tbl:gltbl) (s:steps) =
+    List.iter (fun x -> apply_step slvenv tbl x) s.s
+
+  let unapply_step (slvenv:slvr) (tbl:gltbl) (s:step) =
+  (*let _ = Printf.printf "> undo step %s\n" (step2str s) in*)
+  match s with
+  | SAddGoal(g) -> let _ = SET.rm tbl.goals g in ()
+  | SRemoveGoal(g) -> let _ = SET.add tbl.goals g in ()
+  | SAddNode(id,rels) -> let _ = MAP.rm tbl.nodes id in ()
+  | SSolUseNode(id,i) -> let _ = SlnLib.usecomp_unmark tbl.sln id i in ()
+  | SSolAddConn(src,snk) -> let _ = SlnLib.mkconn_undo tbl.sln src snk in ()
+  | SSolAddLabel(wid, prop, lbl) -> let _ = SlnLib.mklabel_undo tbl.sln wid prop lbl in ()
+
+  let unapply_steps (slvenv) (tbl:gltbl) (s:steps) =
+    List.iter (fun x -> unapply_step slvenv tbl x) s.s
+
+  let move_cursor (s:slvr) (tbl:gltbl) (next:steps) =
+    let b = tbl.search in
+    match b.curr with
+    | Some(old) ->
+      let anc = TREE.ancestor b.paths next old in
+      let to_anc = LIST.sublist (LIST.rev (TREE.get_path b.paths old)) old anc in
+      let from_anc = LIST.sublist (TREE.get_path b.paths next) anc next in
+      let _ = List.iter (fun x -> let _ = unapply_steps s tbl x in ()) to_anc in
+      let _ = List.iter (fun x -> let _ = apply_steps s tbl x in ()) from_anc in
+      let _ = (b.curr <- Some next) in
+      tbl
+    | None -> let to_node = TREE.get_path b.paths next in
+      let _ = List.iter (fun x -> let _ = apply_steps s tbl x in ()) to_node in
+      let _ = (b.curr <- Some next) in
+      tbl
+
+  let get_paths (b:buffer) =
+    let p = TREE.leaves b.paths in
+    let p = List.filter (fun x -> visited b x = false) p in
+    p
+
+  let random_path (b:buffer) =
+    let choices = get_paths b in
+    if List.length choices > 0 then
+      Some (LIST.rand choices)
+    else
+      None
+
+  let mkbuf goals =
+    let g = TREE.make (fun x y -> x.id=y.id) (fun x y -> x = y) in
+    let buf = {paths=g; step_buf=None; cnt=0; curr=None; visited=SET.make (fun x y -> x = y)} in
+    let _ = start buf in
+    let _ = List.iter (fun x -> add_step buf (SAddGoal x)) goals in
+    match buf.step_buf with
+    |Some(sb) ->
+      let _ = TREE.mknode buf.paths (sb) in
+      let _ = TREE.setroot buf.paths (sb) in
+      let _ = (buf.step_buf <- None) in
+      sb,buf
+    |None -> error "mkbuf" "impossible to not have initial step"
+end
---
+++ solver/SolverSln.ml
@@ -0,0 +1,175 @@
+open Util
+open SolverData
+
+open Common
+open HW
+open Math
+open SolverUtil
+open Unit
+open SolverRslv
+
+module SlnLib =
+struct
+  let hwport2wire cm port =
+    match cm with
+    | HCMLocal(c) -> error "hwport2wire" ("underspecified identifier "^c^".")
+    | HCMGlobal(q,i) ->
+      let nid = Shim.name2unodeid q in
+      let inst = i in
+      (nid,inst,port)
+
+  let wire2str wire =
+    let n,i,p = wire in
+    (UnivLib.unodeid2name n)^"["^(string_of_int i)^"]."^p
+
+  let label2str pr =
+    match pr with
+    | LMagnitude(hwrng,hwp,mrng,mp) ->
+      let hwr = (RANGE.tostr hwrng)^" "^(hwp) in
+      let mr = (RANGE.tostr mrng)^" "^(UnitLib.unit2str mp) in
+      "mag "^hwr^" => "^mr
+    | LTime(hwrng,hwp,mrng,mp) ->
+      let hwr = (RANGE.tostr hwrng)^" "^(hwp) in
+      let mr = (RANGE.tostr mrng)^" "^(mp) in
+      "time "^hwr^" => "^mr
+    | LError -> "error prop"
+    | LBindValue(v) -> "bind "^(string_of_float v)
+    | LBindVar(v) -> "bind "^(MathLib.mid2str v)
+
+  let mksln () : sln =
+    {comps=MAP.make();conns=MAP.make(); labels=MAP.make()}
+
+  let mkcomp (sln:sln) (id:unodeid) =
+    MAP.put sln.comps id (SET.make (fun x y -> x = y),0)
+
+  (*
+  let mkconn_valid (v:slvr) (s:sln) (src:wireid) (snk:wireid) =
+    let res = ConnResolver.is_valid v.cstr.conns s.conns in
+    res
+  *)
+
+  let mkconn_cons (v:slvr) (s:sln) =
+    let res = HwConnRslvr.is_valid v s in
+    res
+
+  let mklbl_cons (v:slvr) (s:sln) =
+    let res = HwErrRslvr.is_valid v s in
+    res
+
+  let mkconn (sln:sln) (src:wireid) (snk:wireid) =
+    let sinks = if MAP.has sln.conns src = false then
+        let s = (SET.make (fun x y -> x = y) ) in
+        let _ = MAP.put sln.conns src s in
+        s
+      else
+        MAP.get sln.conns src
+    in
+      let _ = SET.add sinks snk in
+      let _ = MAP.put sln.conns src sinks in
+      ()
+
+  let mkconn_undo (sln:sln) (src:wireid) (snk:wireid) =
+    if MAP.has sln.conns src then
+      let s = MAP.get sln.conns src in
+      let _ = SET.rm s snk in
+      let _ = MAP.put sln.conns src s in
+      ()
+    else
+      error "mkconn_undo" "cannot undo connection that doesn't exist."
+
+  let mklabel (sln:sln) (id:wireid) (prop:propid) (v:label) =
+    let prps = if MAP.has sln.labels id = false then
+        let pmap = MAP.make () in
+        let _ = MAP.put sln.labels id pmap in
+        pmap
+      else
+        MAP.get sln.labels id
+    in
+    let pset = if MAP.has prps prop = false then
+      let pset = SET.make (fun x y -> x = y) in
+      let _ = MAP.put prps prop pset in
+      pset
+      else
+        MAP.get prps prop
+    in
+    let _ = SET.add pset v in
+    ()
+
+  let mklabel_undo (sln:sln) (id:wireid) (prop:propid) (v:label) =
+    if MAP.has sln.labels id then
+      let props = MAP.get sln.labels id in
+      if MAP.has props prop then
+        let lbls = MAP.get props prop in
+        let _ = SET.rm lbls v in
+        ()
+    else
+      ()
+
+  let usecomp (sln:sln) id =
+    let l,n = MAP.get sln.comps id in
+    let _ = sln.comps <- MAP.put sln.comps id (SET.add l n,n+1) in
+    n
+
+  let usecomp_conserve (s:slvr) (sln:sln) id : bool =
+    let lst,_ = (MAP.get sln.comps id)  in
+    let nuses = SET.size lst in
+    let maxuses = Shim.max4unodeid s id in
+    if maxuses >= nuses then true else false
+
+  let usecomp_valid (s:slvr) (sln:sln) id : bool =
+    let lst,_ = (MAP.get sln.comps id)  in
+    let nuses = SET.size lst in
+    let maxuses = Shim.max4unodeid s id in
+    if maxuses > nuses then true else false
+
+  let usecomp_mark (s:sln) id (i:int) =
+    let lst,n = MAP.get s.comps id in
+    let lst = SET.add lst i in
+    s.comps <- MAP.put s.comps id (lst,n)
+
+  let usecomp_unmark (s:sln) id (i:int) =
+    let lst,n = MAP.get s.comps id in
+    let _ = SET.rm lst i in
+    s.comps <- MAP.put s.comps id (lst,n)
+
+
+  let conserve (v:slvr) (s:sln) =
+    if mkconn_cons v s && mklbl_cons v s
+    then true
+    else false
+
+  let tostr (s:sln) : string=
+    let comp2str cname clist id =
+      let instlist2str lst =
+        SET.tostr clist (fun x -> string_of_int x) ","
+      in
+      (UnivLib.unodeid2name cname)^" | "^(string_of_int id)^" = ["^(instlist2str clist)^"]"
+    in
+    let comps2str c =
+      let res : string = MAP.fold c (fun k (l,n) r -> r^"\n"^(comp2str k l n)) "" in
+      res
+    in
+    let conns2str c =
+      let itersnks src sset = SET.fold sset (fun snk r -> r^"\n"^(wire2str src)^"->"^(wire2str snk) ) "" in
+      let res : string = MAP.fold c (fun src snkset r -> (itersnks src snkset)^r) "" in
+      res
+    in
+    let labels2str c =
+      let props2str cmps prp labels =
+        if SET.size labels > 0 then
+          (SET.tostr labels (fun x -> prp^"("^cmps^"): "^(label2str x)) "\n")^"\n"
+        else
+          ""
+      in
+      let lbls2str k props =
+        let cmpstr = wire2str k in
+        (MAP.fold props (fun prp v r -> r^(props2str cmpstr prp v) ) "")
+      in
+      MAP.fold c (fun k v r -> r^(lbls2str k v)) ""
+    in
+    let cstr = comps2str s.comps in
+    let cnstr = conns2str s.conns in
+    let lstr = labels2str s.labels in
+    "Comps:\n"^cstr^"\n\nConns:"^cnstr^"\n\nLabels:\n"^lstr
+
+end
