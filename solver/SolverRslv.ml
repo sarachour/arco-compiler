7b1cd256a6991b59e0e9aa4c409cd404706c04b5
Sara Achour <sachour@mit.edu>
2015-11-06 23:50:01 -0500

    f

--- solver/SolverRslv.ml
+++ solver/SolverRslv.ml
@@ -7,25 +7,99 @@ open SolverUtil
 module HwConnRslvr =
 struct
 
+  type instinfo = {
+      id: int;
+      mutable cinsts: int set;
+      mutable sinsts: int set;
+  }
+  type conninfo = {
+    id: int;
+  }
+
+  type sateqn =
+    |And of sateqn*sateqn
+    |Or of sateqn*sateqn
+    |Var of string
+    |Int of int
+    |Eq of sateqn*sateqn
+
+  let sateqn2str x =
+    let rec _s x = match x with
+    | And(a,b) -> "(and "^(_s a)^" "^(_s b)^")"
+    | Or(a,b) -> "(or "^(_s a)^" "^(_s b)^")"
+    | Eq(a,b) -> "(= "^(_s a)^" "^(_s b)^")"
+    | Var(v) -> v
+    | Int(i) -> string_of_int i
+    | _ -> ""
+    in
+    _s x
+
+  let and_all (x:sateqn list) =
+      match x with
+      | h::h2::t -> List.fold_right (fun x r -> And(x,r)) (h2::t) h
+      | [h] -> h
+      | [] -> error "and_all" "failure"
+
+  let or_all (x:sateqn list) =
+      match x with
+      | h::h2::t -> List.fold_right (fun x r -> Or(x,r)) (h2::t) h
+      | [h] -> h
+      | [] -> error "and_all" "failure"
+
+  let mkinstinfo n =
+    {id=n; cinsts = SET.make (fun x y -> x =y ); sinsts = SET.make (fun x y -> x = y)}
+
   let is_valid cfg sol =
-    let cmps : (unodeid,(int set)*int) map = sol.comps in
-    let conns : (wireid, wireid set) map = sol.conns in
+    let kind_map : (string,instinfo) map= MAP.make() in
+    let mkvar name =
+      if MAP.has kind_map name = false then
+        let v = List.length (MAP.to_values kind_map) in
+        let iifo = mkinstinfo v in
+        let _ = MAP.put kind_map name iifo in
+        ()
+      else
+        ()
+    in
+    let declvar (name:string) id (cstr:bool)=
+      let _ = mkvar (name) in
+      let ifo = MAP.get kind_map name in
+      if cstr then
+        let _ = (ifo.cinsts <- SET.add ifo.cinsts id) in
+        ()
+      else
+        let _ = (ifo.sinsts <- SET.add ifo.sinsts id) in
+        ()
+    in
+    let tovar (name:string) (id) =
+      let ifo = MAP.get kind_map name in
+      "x"^(string_of_int ifo.id)^"x"^(string_of_int (id+1))
+    in
+    let towvar ((u,i,p):wireid) =
+      (tovar (UnivLib.unodeid2name u) i)^"x"^p
+    in
+    let toconnvar (a:wireid) (b:wireid) =
+      (towvar a)^"_"^(towvar b)
+    in
+    let tosmt () =
+      let mapc fn ic = MAP.fold kind_map (fun name iifo r -> SET.fold iifo.cinsts (fun inst r -> fn name inst iifo r ) r) ic in
+      let decls = mapc (fun n i ifo r -> r^"(declare-const "^(tovar n i)^" Int)\n") "" in
+      (*make the clause that forces one assignment*)
+      let slntocstr_asserts : sateqn list = mapc (fun name inst ifo r ->
+          let cid = tovar name inst in
+          let eqs = SET.fold ifo.sinsts (fun j lst -> Eq(Var(cid),Int(j+1))::lst) [Eq(Var(cid),Int(0))] in
+            (or_all eqs)::r) []
+      in
+      let _ = Printf.printf "%s\n" decls in
+      let _ = List.iter (fun q -> Printf.printf "(assert %s)\n" (sateqn2str q)) slntocstr_asserts in
+      ()
+    in
+    let sol_cmps : (unodeid,(int set)*int) map = sol.comps in
+    let sol_conns : (wireid, wireid set) map = sol.conns in
     let cstr_cmps : (string,hcinst) map= cfg.hw.cstr.insts in
-    let vc_map = MAP.make() in
-    let vs_map = MAP.make() in
-    let print_sol_inst id iid =
-      let name = UnivLib.unodeid2name id in
-      let _ = Printf.printf "SOL VAR || %s %d\n" name iid in
-      ()
-    in
-    let print_cstr_inst name iid=
-      let _ = Printf.printf "CSTR VAR || %s %d\n" name iid in
-      ()
-    in
-    let iter_sol_insts fn = MAP.iter cmps (fun k (v,cnt) -> SET.iter v (fun x -> fn k x)) in
-    let iter_cstr_insts fn = MAP.iter cstr_cmps (fun k q -> List.iter (fun x -> fn k x) (LIST.mkrange 1 q)) in
-    let _ = iter_sol_insts print_sol_inst in
-    let _ = iter_cstr_insts print_cstr_inst in
+    let cstr_conns : (string*string,hcconn) map = cfg.hw.cstr.conns in
+    let _ = MAP.iter sol_cmps (fun k (v,cnt) -> SET.iter v (fun x  -> declvar (UnivLib.unodeid2name k) x false) ) in
+    let _ = MAP.iter cstr_cmps (fun k q -> List.iter (fun x -> declvar k x true) (LIST.mkrange 1 q) ) in
+    let _ = tosmt () in
     true
 
 end
