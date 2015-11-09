30dd09a08ed3d8e2f0114a5bc58ce16a7a2860ff
Sara Achour <sachour@mit.edu>
2015-11-09 16:18:27 -0500

    f

--- sim/sim.py
+++ sim/sim.py
@@ -2,6 +2,15 @@
 
 import argparse
 import subprocess
+import sys
+
+class Analysis:
+
+    def get_initial_analysis(self):
+        return ".OP\n"
+
+    def get_dc_analysis(self,name,prop):
+        return ".print DC "+prop+"("+name+")\n"
 
 def parse_args ():
     parser = argparse.ArgumentParser(description='Simulate the topology')
@@ -12,6 +21,32 @@ def parse_args ():
 
     args = parser.parse_args()
     return args
+
+def conc_inputs(text,inps):
+
+    sub = lambda q,m : q.replace(m,"1.0")
+
+    for input in inps:
+        ky = inps[input]["var"]
+        text=sub(text,ky)
+
+    return text
+
+def conc_outputs(aly,text,outs):
+
+    append = lambda x:  text+x
+
+    aline = aly.get_initial_analysis()
+    text = append(aline)
+    
+    for oname in outs:
+        port = outs[oname]["port"]
+        prop = outs[oname]["prop"]
+
+        aline = aly.get_dc_analysis(str(port),prop)
+        text = append(aline)
+
+    return text
 
 def gen_spice(args):
     circ = (args.circuit)
@@ -25,10 +60,45 @@ def gen_spice(args):
     include_st=".include "+lib+"\n"
     outs = append(include_st)
 
-    for line in fcirc:
-        outs = append(line)
+    outputs = {}
+    inputs = {}
+    outname = None
+    inname = None
+    icnt = 1
 
-    return outs
+    for line in fcirc:
+
+        if line.startswith("*@"):
+            # parse command in pragma
+            cmd = line.strip("\n").split("@")[1]
+            cmdargs = cmd.split(" ")
+            cmd = cmdargs[0];
+            # determine the kind of command
+            if cmd == "input":
+                inname = cmdargs[1]
+                if not (inname in inputs):
+                    mvar = "@"+str(icnt)+"@"
+                    inputs[inname] = {"var":mvar}
+                    icnt += 1
+
+            elif cmd == "output":
+                outname = cmdargs[1]
+                outputs[outname] = {"prop":None,"port":None}
+
+            elif cmd == "args":
+                if outname != None:
+                    outputs[outname]["port"] = int(cmdargs[1])
+                    outputs[outname]["prop"] = cmdargs[2]
+                    outname = None
+        else:
+            if inname != None:
+                line = line.replace("#",inputs[inname]["var"])
+                inname = None
+
+            outs = append(line)
+
+    return outputs,inputs,outs
+
 
 def run_spice(text):
     tmpfile = "___tmp___.ckt"
@@ -39,6 +109,9 @@ def run_spice(text):
 
     subprocess.call(["ngspice","-b",tmpfile])
 
+aly = Analysis()
 args = parse_args()
-spc = gen_spice(args)
+oar,iar,spc = gen_spice(args)
+spc = conc_inputs(spc,iar)
+spc = conc_outputs(aly,spc,oar)
 run_spice(spc)
