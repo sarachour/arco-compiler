46898497437e48aa140aa83f042387393fae3cca
Sara Achour <sachour@mit.edu>
2015-11-09 16:38:51 -0500

    works

--- sim/sim.py
+++ sim/sim.py
@@ -3,6 +3,7 @@
 import argparse
 import subprocess
 import sys
+import random
 
 class Analysis:
 
@@ -12,22 +13,25 @@ class Analysis:
     def get_dc_analysis(self,name,prop):
         return ".print DC "+prop+"("+name+")\n"
 
+def rand(min,max):
+    return (random.random()*(max-min) + min)
+
 def parse_args ():
     parser = argparse.ArgumentParser(description='Simulate the topology')
 
 
     parser.add_argument("circuit", help="circuit specification")
     parser.add_argument("--lib", help="library to link with circuit specification")
+    parser.add_argument("--inputs", help="input values")
 
     args = parser.parse_args()
     return args
 
 def conc_inputs(text,inps):
-
-    sub = lambda q,m : q.replace(m,"1.0")
+    sub = lambda q,m : q.replace(m["var"],str(m["val"]))
 
     for input in inps:
-        ky = inps[input]["var"]
+        ky = inps[input]
         text=sub(text,ky)
 
     return text
@@ -38,7 +42,7 @@ def conc_outputs(aly,text,outs):
 
     aline = aly.get_initial_analysis()
     text = append(aline)
-    
+
     for oname in outs:
         port = outs[oname]["port"]
         prop = outs[oname]["prop"]
@@ -78,7 +82,7 @@ def gen_spice(args):
                 inname = cmdargs[1]
                 if not (inname in inputs):
                     mvar = "@"+str(icnt)+"@"
-                    inputs[inname] = {"var":mvar}
+                    inputs[inname] = {"var":mvar,"val":rand(-5,5)}
                     icnt += 1
 
             elif cmd == "output":
@@ -97,8 +101,25 @@ def gen_spice(args):
 
             outs = append(line)
 
+    fcirc.close()
+
     return outputs,inputs,outs
 
+def get_input_params(args, ips):
+    if args.inputs == None:
+        return iar
+
+    ipfile = (args.inputs)
+
+    iph = open(ipfile,'r')
+
+    for line in iph:
+        args = line.strip("\n").split(" ")
+        inp = args[0]
+        val = args[1]
+        ips[inp]["val"] = val
+
+    return iar
 
 def run_spice(text):
     tmpfile = "___tmp___.ckt"
@@ -112,6 +133,8 @@ def run_spice(text):
 aly = Analysis()
 args = parse_args()
 oar,iar,spc = gen_spice(args)
+iar = get_input_params(args,iar)
+
 spc = conc_inputs(spc,iar)
 spc = conc_outputs(aly,spc,oar)
 run_spice(spc)
