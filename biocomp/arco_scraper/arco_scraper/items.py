e74e5c31203ea1348a9eba304ef72bae6a34b642
Nick Charchut <nickcharchut@31-33-58.wireless.csail.mit.edu>
2016-01-07 16:20:54 -0500

    scraper almost done

---
+++ biocomp/arco_scraper/arco_scraper/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class ArcoScraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    Publication_ID = scrapy.Field() # DONE
+    BIO_ID = scrapy.Field() # DONE
+    URL = scrapy.Field() # DONE
+    Submission_Date = scrapy.Field() # DONE
+    problem_size = scrapy.Field() #MIGHT HAVE TO DO SOME POST PROCESSING FOR THIS PART
+    number_of_citations = scrapy.Field() # SECOND SPIDER NEEDED
+    bibtex_citation = scrapy.Field() # SECOND SPIDER NEEDED
+    file_urls = scrapy.Field()  # DON'T TOUCH, NECESSARY FOR FILE DOWNLOAD
+    files = scrapy.Field() # SAME AS ABOVE
---
+++ biocomp/arco_scraper/arco_scraper/pipelines.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.files import FilesPipeline
+from arco_scraper import settings
+
+	 
+class MyFilesPipeline(FilesPipeline):
+    def file_path(self, request, response=None, info=None):
+        media_guid = request.url[50:65]
+        return '%s/%s' % (media_guid, media_guid)
+
+
+def write_to_csv(item):
+	outputfilename = 'Downloads/'+item['BIO_ID'] + '/' + item['BIO_ID']
+	desired_stats = sorted(['Submission_Date', 'BIO_ID', 'Publication_ID', 'URL'])
+	f = open(outputfilename, 'wb')
+	for i in desired_stats:
+		f.write(i +' : '+ item[i] + '\n')
+	f.close()
+
+
+class WriteToCsv(object):
+    def process_item(self, item, spider):
+        write_to_csv(item)
+        return item
