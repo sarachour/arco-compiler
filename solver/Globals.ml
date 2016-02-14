7654cb23400a73b54c4387d7f9cb3c7609365070
Sara Achour <sarachour@gmail.com>
2016-02-14 01:37:49 -0500

    G

--- solver/Globals.ml
+++ solver/Globals.ml
@@ -1,6 +1,6 @@
-let __max_depth = 65
-let __pattern_depth = 5
+let __max_depth = 36
+let __pattern_depth = 8
 
-let __ast_pattern_breadth = 15
-let __ast_pattern_depth = 2
-let __ast_pattern_frac_ban = 0.65
+let __ast_pattern_breadth = 5
+let __ast_pattern_depth = 5
+let __ast_pattern_frac_ban = 0.40
