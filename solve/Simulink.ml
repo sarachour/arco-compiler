aace993b6db2e746eff7df58cd5b8ae0838018c1
Sara Achour <sachour@mit.edu>
2016-11-03 05:36:23 -0400

    test partial

--- solve/Simulink.ml
+++ solve/Simulink.ml
@@ -135,7 +135,7 @@ struct
         declare_var loc;
         q (add_block (get_basic_fxn "in") loc);
         q (set_param (MATLit(MATStr(loc))) "Name"
-             (MATVar(MATScalar(vr))) )
+             (MATLit(MATStr(vr))) )
       end;
     loc
 
