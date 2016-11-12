6e251a3688fc4f8c157775700e2712a258d00b3b
Sara Achour <sachour@mit.edu>
2016-11-12 14:34:05 -0500

    T

--- solve/output/biology/mmrxn/with-map/killall.sh
+++
@@ -1,9 +0,0 @@
-
-
-ARG=$1
-
-for p in $(ps -eocomm,pid | grep  $ARG | sed "s/$ARG\s*//g")
-do
-  echo "killing $p"
-  kill -9 $p
-done
--- solve/output/biology/mmrxn/with-map/timebomb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CMD=$1
-WAITTIME=$2
-echo "#!/bin/bash" > _script.sh
-echo $CMD >> _script.sh
-chmod +x _script.sh
-
-timeout ${WAITTIME}s ./_script.sh
